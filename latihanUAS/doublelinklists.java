b43d8a5f5295a84a34b36d6e8722733ec88eab07
dinaKSari <dinakumala7070@gmail.com>
2025-06-18 23:26:17 +0700

    membuat method

--- latihanUAS/doublelinklists.java
+++ latihanUAS/doublelinklists.java
@@ -1,12 +1,13 @@
 package latihanUAS;
+
 public class doublelinklists {
     int max, size;
-    node head, rear;
+    node head, tail;
 
     public doublelinklists(int max) {
         this.max = max;
         car car1[] = new car[max];
-        
+        this.head = this.tail = null;
     }
 
     public boolean isEmpty(){
@@ -31,7 +32,42 @@ public class doublelinklists {
             System.out.println("Isi penuh!");
         }
         if(isEmpty()){
-            head = rear = c1;
+            head = tail = newNode;
+        } else{
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
+        System.out.println("mobil ditambahkan!");
+    }
+
+    public car kurang(){
+        if(isEmpty()){
+            System.out.println("antrian kosong!");
+        }
+        car temp = head.data;
+        head = head.next;
+        if(head != null){
+            head.prev = null;
+        } else{
+            tail = null;
+        }
+        size--;
+        System.out.println("Mobil " + temp.plat + " sudah dikeluarkan!");
+        return temp;
+    }
+
+    public void tampilSemua(){
+        if(isEmpty()){
+            System.out.println("kosong!");
+        }
+        node temp = head;
+        while(head != null){
+            if(temp == null){
+                break;
+            }
+            temp.data.infoCar();
+            temp = temp.next;
         }
     }
 }
